6ce1caeda40642dacc032043df45083233604885
Huimin <huhuimin236@gmail.com>
2023-08-24 15:42:23 +0200

    Changing code (these changes lead a useless suppression: # pylint: disable=unused-argument)

--- src/calculate.py
+++ src/calculate.py
@@ -10,7 +10,10 @@ def main(operation):
     # pylint: disable=unused-argument
     x = "3" # type: ignore
     y = 5
-    print(add(x, y)) 
+    if operation == "add":
+        print(add(x, y)) 
+    elif operation == "subtraction":
+        print(subtraction(x, y)) 
 
 if __name__ == "__main__":
     operation = "add"
