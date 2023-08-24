464f7c8da555169c4d0a835c26deacbc7d2f2ee3
Huimin <huhuimin236@gmail.com>
2023-08-24 15:24:09 +0200

    Adding a Pylint suppression

---
+++ src/calculate.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    operator = "" # pylint: disable=unused-variable
+    result = a + b  
+    return result
+
+def main():
+    x = 3
+    y = 5
+    print(add(x, y)) 
+
+if __name__ == "__main__":
+    main()
